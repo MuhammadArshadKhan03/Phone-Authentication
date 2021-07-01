9e080930fdf88db97b327a76893d0859fa22f7cc
Arshad Khan <83055314+MuhammadArshadKhan03@users.noreply.github.com>
2021-07-01 16:52:35 +0500

    Google Auth
    Here is google authentication

---
+++ lib/Google Auth/Next_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class nextpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Welcome Here",
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
